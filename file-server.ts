cddfa23353afe56b1b4859680edabe8e20c34e05
Alejandro Solano <54044412+soranoalejandro@users.noreply.github.com>
2025-01-24 07:58:20 -0400

    Read permissions to ./public folder only
    visit: http://localhost:8080/public/home.html to test.

--- file-server.ts
+++ file-server.ts
@@ -1,19 +1,19 @@
-const availablePaths = [ "about.txt" ]
+// this function will handle the http string request
+async function handler(request: Request) {
+  console.log(request.url);
+  
+  const url = new URL(request.url);
+  const filepath = decodeURIComponent(url.pathname);
 
+  try {
+    const file = await Deno.open("." + filepath, { read: true });
+    //console.log(filepath);
+    return new Response(file.readable);
+  } catch {
+    console.log(filepath, 404);
+    return new Response("404 Not Found", { status: 404 });
+  }
+}
 
-Deno.serve(
-  { hostname: "localhost", port: 8080 },
-  async (request) => {
-    const url = new URL(request.url);
-    const filepath = decodeURIComponent(url.pathname);
-    
-    try {
-      const file = await Deno.open("." + filepath, { read: true });
-      console.log(filepath);
-      return new Response(file.readable);
-    } catch {
-      console.log(filepath, 404);
-      return new Response("404 Not Found", { status: 404 });
-    }
-  },
-);
+const options = { port: 8080 };
+Deno.serve(options, handler);
